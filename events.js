96530f61be32f2d708a598543d8ffac44f6a3ce3
Brad <bradleyjplatt@yahoo.com>
2024-04-18 19:14:11 -0700

    Add a standard sb handler for multiple events

--- events.js
+++ events.js
@@ -28,13 +28,15 @@ sync.addTemporaryListener(
 			case "MESSAGE_CREATE": {
 				if (data.d.author.bot) return
 
-				if (!data.d.guild_id) return
-				/** @type {DBStarboards | undefined} */
-				const sb = await db.get("SELECT * FROM starboards WHERE guild_id =?", [data.d.guild_id])
-				if (sb && !sb.ignore_channel_ids?.split(",").includes(data.d.channel_id)) utils.setCachedObject("message", data.d.id, data.d, 1000 * 60 * 60 * 2)
+				starboardMessageHandler("create", data.d)
 
 				if (utils.checkTriggers(data.d)) return
 				if (await utils.checkCrashLog(data.d)) return
+				break
+			}
+
+			case "MESSAGE_UPDATE": {
+				starboardMessageHandler("update", data.d)
 				break
 			}
 
@@ -45,90 +47,12 @@ sync.addTemporaryListener(
 			}
 
 			case "MESSAGE_REACTION_ADD": {
-				/** @type {import("discord-api-types/v10").APIMessage | null} */
-				const cached = utils.getCachedObject("message", data.d.message_id)
-				if (cached) {
-					if (cached.author.id === data.d.user_id) return snow.channel.createMessage(data.d.channel_id, { content: `🚨🚨 <@${data.d.user_id}> IS A THOT AND SELF-STARRED THEIR MEME 🚨🚨` })
-
-					const existingReaction = cached.reactions?.find(r => r.emoji.name === data.d.emoji.name)
-					if (!existingReaction) {
-						if (!cached.reactions) cached.reactions = []
-						cached.reactions.push({ count: 1, count_details: { burst: 0, normal: 0 }, me: false, me_burst: false, emoji: data.d.emoji, burst_colors: [] })
-					} else existingReaction.count++
-				}
-
-				if (!data.d.member?.user) return
-				if (data.d.member.user.bot) return
-
-				/** @type {DBStarboards | undefined} */
-				const sb = await db.get("SELECT * FROM starboards WHERE guild_id =?", [data.d.guild_id])
-				if (!sb) return
-
-				if (sb.emoji !== data.d.emoji.name) return
-				const ignored = sb.ignore_channel_ids?.split(",") ?? []
-				if (ignored.includes(data.d.channel_id)) return
-
-				const message = cached ?? await utils.fetchObjectWithCache(getChannelMessage, "message", data.d.message_id, 1000 * 60 * 60 * 12, data.d.channel_id, data.d.message_id)
-				if (!message?.reactions) return
-				if (message.author.bot) return
-
-				const reaction = message.reactions.find(r => r.emoji.name === data.d.emoji.name)
-				if (!reaction) return
-
-				const instantPromote = !!(sb.instant_promote_role_ids?.split(",") ?? []).find(r => data.d.member?.roles.includes(r))
-
-				/** @type {DBStarboardMap | undefined} */
-				const existingPost = await db.get("SELECT * FROM starboard_map WHERE message_id =?", [data.d.message_id])
-				const content = utils.replace(starboardContentFormat, { "emoji": sb.emoji, "reactions": reaction.count, "jump": `https://discord.com/channels/${data.d.guild_id}/${data.d.channel_id}/${data.d.message_id}` })
-				if (existingPost) snow.channel.editMessage(sb.channel_id, existingPost.sb_message_id, { content })
-				else if (reaction.count >= sb.min || instantPromote) {
-					const result = await snow.channel.createMessage(sb.channel_id, {
-						content,
-						embeds: [
-							{
-								author: {
-									name: message.author.username
-								},
-								description: message.content.length ? message.content : undefined,
-								image: message.attachments.length ? { url: message.attachments[0].url, proxy_url: message.attachments[0].proxy_url } : undefined
-							}
-						]
-					})
-					db.all("INSERT INTO starboard_map (message_id, sb_message_id) VALUES (?, ?)", [message.id, result.id])
-				}
+				starboardMessageHandler("add", data.d)
 				break
 			}
 
 			case "MESSAGE_REACTION_REMOVE": {
-				/** @type {import("discord-api-types/v10").APIMessage | null} */
-				const cached = utils.getCachedObject("message", data.d.message_id)
-				if (cached) {
-					if (cached.author.id === data.d.user_id) return
-					const existingReaction = cached.reactions?.find(r => r.emoji.name === data.d.emoji.name)
-					if (existingReaction) existingReaction.count--
-				}
-
-				/** @type {DBStarboards | undefined} */
-				const sb = await db.get("SELECT * FROM starboards WHERE guild_id =?", [data.d.guild_id])
-				if (!sb) return
-
-				if (sb.emoji !== data.d.emoji.name) return
-				const ignored = sb.ignore_channel_ids?.split(",") ?? []
-				if (ignored.includes(data.d.channel_id)) return
-
-				/** @type {DBStarboardMap | undefined} */
-				const existingPost = await db.get("SELECT * FROM starboard_map WHERE message_id =?", [data.d.message_id])
-
-				if (existingPost) {
-					const message = cached ?? await utils.fetchObjectWithCache(getChannelMessage, "message", data.d.message_id, 1000 * 60 * 60 * 12, data.d.channel_id, data.d.message_id)
-					if (!message?.reactions) return
-
-					const reaction = message.reactions.find(r => r.emoji.name === data.d.emoji.name)
-					if (!reaction) return
-
-					const content = utils.replace(starboardContentFormat, { "emoji": sb.emoji, "reactions": reaction.count, "jump": `https://discord.com/channels/${data.d.guild_id}/${data.d.channel_id}/${data.d.message_id}` })
-					snow.channel.editMessage(sb.channel_id, existingPost.sb_message_id, { content })
-				}
+				starboardMessageHandler("remove", data.d)
 				break
 			}
 
@@ -136,6 +60,120 @@ sync.addTemporaryListener(
 		}
 	}
 )
+
+/**
+ * For some reason keyof <RecordHere> doesn't work so this is necessary
+ * @template T
+ * @typedef {T extends Record<infer R, any> ? R : never} RKeys
+ */
+
+/**
+ * @template {{
+ * 	"create": import("discord-api-types/v10").GatewayMessageCreateDispatchData,
+ * 	"update": import("discord-api-types/v10").GatewayMessageUpdateDispatchData,
+ * 	"add": import("discord-api-types/v10").GatewayMessageReactionAddDispatchData,
+ * 	"remove": import("discord-api-types/v10").GatewayMessageReactionRemoveDispatchData
+ * }} T
+ * @template {RKeys<T>} D
+ * @param {D} mode
+ * @param {T[RKeys<T>]} data
+ */
+async function starboardMessageHandler(mode, data) {
+	/** @type {T["create"]} */ // @ts-ignore
+	const create = data,
+	/** @type {T["update"]} */ // @ts-ignore
+	update = data,
+	/** @type {T["add"]} */ // @ts-ignore
+	add = data,
+	/** @type {T["remove"]} */ // @ts-ignore
+	remove = data // yes this is necessary. Type narrowing wouldn't work
+
+	let guildID, channelID, messageID, userID
+
+	switch (mode) {
+		case "add":
+			guildID = add.guild_id; channelID = add.channel_id; messageID = add.message_id; userID = add.user_id
+			break
+		case "remove":
+			guildID = remove.guild_id; channelID = remove.channel_id; messageID = remove.message_id; userID = remove.user_id
+			break
+		case "update":
+			guildID = update.guild_id; channelID = update.channel_id; messageID = update.id; userID = update.member?.user?.id ?? update.author?.id
+			break
+		case "create":
+			guildID = create.guild_id; channelID = create.channel_id; messageID = create.id; userID = create.author.id
+			break
+		default: throw new Error("No")
+	}
+
+	if (!guildID) return
+
+	/** @type {DBStarboards | undefined} */
+	const sb = await db.get("SELECT * FROM starboards WHERE guild_id =?", guildID)
+	if (!sb) return
+
+	if (sb.ignore_channel_ids?.split(",").includes(channelID)) return
+
+	if (mode === "create") return utils.setCachedObject("message", create.id, create, 1000 * 60 * 60 * 6)
+
+	/** @type {import("discord-api-types/v10").APIMessage | null} */
+	const cached = utils.getCachedObject("message", messageID)
+
+	if (mode === "update" && cached) Object.assign(cached, update)
+
+	const message = cached ?? await utils.fetchObjectWithCache(getChannelMessage, "message", messageID, 1000 * 60 * 60 * 6, channelID, messageID)
+	if (!message) return
+	if (!message.reactions) message.reactions = []
+
+
+	if (mode === "add" || mode === "remove") {
+		if (sb.emoji !== (add ?? remove).emoji.name) return
+	}
+
+	if (mode === "add" && message.author.id === userID) return snow.channel.createMessage(channelID, { content: `🚨🚨 <@${userID}> IS A THOT AND SELF-STARRED THEIR MEME 🚨🚨` })
+	if (mode === "add" || mode === "remove") {
+		const existingReaction = message.reactions.find(r => r.emoji.name === (add ?? remove).emoji.name) // add ?? remove for type safety
+		if (!existingReaction && mode === "add") message.reactions.push({ count: 1, count_details: { burst: 0, normal: 0 }, me: false, me_burst: false, emoji: add.emoji, burst_colors: [] })
+
+		if (existingReaction) {
+			if (mode === "add") existingReaction.count++
+			else existingReaction.count--
+		}
+	}
+
+	const reaction = message.reactions.find(r => r.emoji.name === sb.emoji)
+	if (!reaction) return
+
+	const existingEmbedFromMessage = message.embeds.find(e => !!e.image?.url)
+
+	/** @type {import("discord-api-types/v10").APIEmbed} */
+	const embed = {
+		author: {
+			name: message.author.username
+		},
+		description: message.content.length
+			? message.content.slice(0, 1999)
+			: undefined,
+		image: message.attachments.length
+			? { url: message.attachments[0].url, proxy_url: message.attachments[0].proxy_url }
+			: existingEmbedFromMessage?.image
+				? { url: existingEmbedFromMessage.image.url, proxy_url: existingEmbedFromMessage.image.proxy_url } // in the case of message updates with embeds from users
+				: undefined
+	}
+
+	/** @type {DBStarboardMap | undefined} */
+	const existingPost = await db.get("SELECT * FROM starboard_map WHERE message_id =?", [messageID])
+	const content = utils.replace(starboardContentFormat, { "emoji": sb.emoji, "reactions": reaction.count, "jump": `https://discord.com/channels/${guildID}/${channelID}/${messageID}` })
+
+	if (!existingPost) {
+		const instantPromote = !!sb.instant_promote_role_ids?.split(",").find(r => add.member?.roles.includes(r))
+		if (reaction.count >= sb.min || instantPromote) {
+			const result = await snow.channel.createMessage(sb.channel_id, { content, embeds: [embed] })
+			db.all("INSERT INTO starboard_map (message_id, sb_message_id) VALUES (?, ?)", [message.id, result.id])
+			return
+		}
+	} else snow.channel.editMessage(sb.channel_id, existingPost.sb_message_id, { content, embeds: [embed] })
+}
 
 /**
  * @typedef {{ guild_id: string, channel_id: string, ignore_channel_ids: string | null, emoji: string, min: number, instant_promote_role_ids: string | null }} DBStarboards
