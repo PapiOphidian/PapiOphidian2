6b3436a4c1b13fe7eb29908626e2935fba5a1f79
Brad <bradleyjplatt@yahoo.com>
2024-06-21 00:52:53 -0700

    Add PhysMod read guidelines command

--- commands.js
+++ commands.js
@@ -35,5 +35,28 @@ commands.assign([
 			const message = cmd.data.messages.get(cmd.target)
 			events.triggerMap["phys_pojav"].trigger(message)
 		}
+	},
+	{
+		name: "read-guidelines",
+		description: "Tell someone to read the before you post guidelines",
+		category: "triggers",
+		type: 1,
+		guild_ids: [events.physModGuildID],
+		options: [
+			{
+				name: "user",
+				type: 6,
+				description: "A user to mention along with the message",
+				required: false
+			}
+		],
+		/** @param {import("./packages/commands").ChatInputCommand} cmd */
+		process(cmd) {
+			const user = cmd.data.users.get(cmd.data.options.get("user")?.asString() ?? "") ?? null
+
+			snow.interaction.editOriginalInteractionResponse(cmd.application_id, cmd.token, {
+				content: `${user ? `<@${user.id}>, s` : "S"}omeone thinks you should read this: https://discord.com/channels/231062298008092673/1115343312417718323/1115343312417718323`
+			})
+		}
 	}
 ])
